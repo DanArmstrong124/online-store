9c246165cc1f9284fdfb42e51f1ae3a93b38337c
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-06 11:49:58 +0000

    Added Stripe

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -62,8 +62,10 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             'templates',
-            'accounts/templates'
-            'ecommerce/templates'
+            'accounts/templates',
+            'ecommerce/templates',
+            'cart/templates',
+            'products/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -141,5 +143,9 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
